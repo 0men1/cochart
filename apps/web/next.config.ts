f8810ed5b0958f321ce4fb0c2adf5bd2c2636f95
0men1 <homahomen2@gmail.com>
2025-12-19 13:27:37 -0600

    fix/wrong api route

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [{
       source: '/api/:path*',
-      destination: `${process.env.SERVER_URL || 'http://localhost:8080'}/:path*`,
+      destination: `${process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080'}/:path*`,
     }]
   }
 };
